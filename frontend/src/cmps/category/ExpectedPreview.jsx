c5ad0045f874bc30ea4349bf3ad2b1ba04cb4756
hadas <hadass91@gmail.com>
2022-04-15 00:43:57 +0300

    category sum row improvements

--- frontend/src/cmps/category/ExpectedPreview.jsx
+++ frontend/src/cmps/category/ExpectedPreview.jsx
@@ -15,18 +15,11 @@ export const ExpectedPreview = ({ expected, color, expensesSum, toggleEditExpect
                         className="inner flex center"
                         style={{ backgroundColor: `var(--${color})`, width: width < 100 ? `${width}%` : '100%' }} >
                     </section>}
-                <span className="txt flex justify-center align-center ">
+                <span className="txt flex justify-center align-center">
                     {!isHover ? <span>${expensesSum} \ ${expected}</span> :
                         <span >{width.toFixed()}%</span>}
                 </span>
             </section>
-            {/* <section className="outter"  >
-                {!!expensesSum && <section className="inner flex center"
-                    style={{ backgroundColor: `var(--${color})`, width: width < 100 ? `${width}%` : '100%' }} >
-                    <span > Total {expensesSum}$ <span className="small">({width.toFixed()}%)</span> </span>
-                </section>}
-            </section> */}
-            {/* <span className="expected-sum" onClick={toggleEditExpected} >Budget: {expected}$</span> */}
         </section>
     )
 }
