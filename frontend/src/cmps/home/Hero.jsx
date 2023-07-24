b582090da7a55e3a9a2774af7a5a18408f3c9093
hadas <hadass91@gmail.com>
2023-07-24 18:01:20 +0300

    homepage header and hero

--- frontend/src/cmps/home/Hero.jsx
+++ frontend/src/cmps/home/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
             <section className="hero">
                 <Sky />
                 <h1 className='main-title' >A platform built for a new way of budgeting</h1>
-                <h2>it so nice to <span>budget</span> with homefin.com </h2>
+                <h2>it so nice to <span className='bold-me'> <span className='underline'></span> budget</span> with homefin.com </h2>
                 <Link to="account/626666dc17d1973db880acc6/6454cdc2f09775418c23ad3a">
                     <button className='see-demo-btn flex'>
                         <span>Get started</span>
